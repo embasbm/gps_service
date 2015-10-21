e1a79def67425a11e086ec4bbc82ff73db204be3
Emba Moussa <emoussa@intellicentrics.com>
2015-10-21 18:31:17 -0500

    make gpspoint model geo mappable

--- app/models/gps_point.rb
+++ app/models/gps_point.rb
@@ -1,4 +1,9 @@
 class GpsPoint < ActiveRecord::Base
   validates :latitude, uniqueness: {scope: :longitude}, :presence => true
   belongs_to :trace
+
+  acts_as_mappable :distance_field_name => :distance,
+                   :lat_column_name => :latitude,
+                   :lng_column_name => :longitude
+
 end

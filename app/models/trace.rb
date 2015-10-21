43016731804a64624164911a55c501fca60c3416
Emba Moussa <emoussa@intellicentrics.com>
2015-10-21 18:30:30 -0500

    instance method to calculate distances for trace gps points

--- app/models/trace.rb
+++ app/models/trace.rb
@@ -1,3 +1,15 @@
 class Trace < ActiveRecord::Base
   has_many :gps_points
+
+  MILES_TO_METERS = 0.00062137
+
+  def get_distances
+    pgs_points_collection = []
+    start_point = gps_points.first
+    gps_points.each do |point|
+      distance = (point.distance_to(start_point) / MILES_TO_METERS).floor
+      pgs_points_collection << { latitude: point[:latitude], longitude: point[:longitude], distance: distance}
+    end
+    pgs_points_collection
+  end
 end
